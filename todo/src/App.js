fa1b2691edc4a1ad56d1cb2ca65aed204fb691c6
Nik200309 <nikitapidlypnij@gmail.com>
2024-10-20 11:24:04 +0300

    first commit

---
+++ todo/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+        <a>Home</a>
+        <p>Date/Calender</p>
+        <div>
+          <p>Register</p><p>/</p><p>Login</p>
+        </div>
+        
+        <input type="text" placeholder="Search.."></input>  
+        </nav>
+      </header>
+      <body className='App-body'>
+        <p>h</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
